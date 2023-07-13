c4add6f4c3a50a049f5eddc4c7b6482f042b31b7
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2023-07-13 19:06:02 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 input.onPinPressed(TouchPin.P0, function () {
     basic.showString("a")
+    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     radio.sendString("a")
 })
 input.onButtonPressed(Button.A, function () {
@@ -7,12 +8,14 @@ input.onButtonPressed(Button.A, function () {
 })
 input.onPinPressed(TouchPin.P2, function () {
     basic.showString("c")
+    music.play(music.tonePlayable(330, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     radio.sendString("c")
 })
 input.onPinPressed(TouchPin.P1, function () {
     basic.showString("b")
+    music.play(music.tonePlayable(294, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
     radio.sendString("b")
 })
-radio.setGroup(1)
+radio.setGroup(3)
 radio.setTransmitPower(7)
 basic.showIcon(IconNames.Happy)
